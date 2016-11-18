03c63c556282d010b1293bfb5cd02734c3985a4d
Mathias Aichinger <mathias.aichinger@runtastic.com>
2016-11-18 19:45:39 +0100

    Setup initial project structure

---
+++ Mobile/talker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  talker
+//
+//  Created by Mathias Aichinger on 18/11/2016.
+//  Copyright © 2016 Easysolutions. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
